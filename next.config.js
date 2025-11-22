e8abdd6494e0197214a00f98c8803930e7d5bc15
pizza3142 <147823957+pizza3142@users.noreply.github.com>
2025-11-22 22:08:15 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' }
+    ]
+  }
+}
+module.exports = nextConfig
